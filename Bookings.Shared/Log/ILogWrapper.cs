8778f6b53ace9c100f7d54e166e1490324e64e98
Shruti Puranik <spuranik@synetec.co.uk>
2022-08-08 00:09:20 +0100

    added log folder

---
+++ Bookings.Shared/Log/ILogWrapper.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookings.Shared.Log
+{
+    public interface ILogWrapper
+    {
+        void Error(Exception ex);
+        void Error(string message, Exception ex);
+        void Info(string info);
+        void Fatal(Exception ex);
+        void Debug(string message);
+    }
+}
